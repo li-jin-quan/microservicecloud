2cffead8afe334de5b829b5ad21913130067fefc
unknown <18617339965@163.com>
2018-11-27 21:52:04 +0800

    20181127

--- microservicecloud/microservicecloud-consumer-dept-80/src/main/java/com/atguigu/springcloud/controller/DeptControllerConsumer.java
+++ microservicecloud/microservicecloud-consumer-dept-80/src/main/java/com/atguigu/springcloud/controller/DeptControllerConsumer.java
@@ -3,6 +3,7 @@ package com.atguigu.springcloud.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,10 +12,11 @@ import org.springframework.web.client.RestTemplate;
 import com.atguigu.springcloud.entites.Dept;
 
 @RestController
+@EnableEurekaClient
 public class DeptControllerConsumer {
 	
-	private static final String REST_URL_PREFIX="http://localhost:8001/";
-
+	//private static final String REST_URL_PREFIX="http://localhost:8001/";
+	private static final String REST_URL_PREFIX = "http://MICROSERVICECLOUD-DEPT/";
 	@Autowired
 	private RestTemplate restTemplate;
 	
@@ -32,5 +34,11 @@ public class DeptControllerConsumer {
 	@RequestMapping(value="/consumer/list")
 	public List<Dept> list(){
 		return restTemplate.getForObject(REST_URL_PREFIX+"dept/list", List.class);
+	}
+	
+	
+	@RequestMapping(value="/consumer/dept/discovery")
+	public Object discovery(){
+		return restTemplate.getForObject(REST_URL_PREFIX+"dept/discovery", Object.class);
 	}
 }
